d606e6bfe114f52ccb72b495902311246f05ea36
MrBabalafe <walkersakatch@gmail.com>
2018-09-25 11:58:30 -0600

    Send photos of items

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def fortnite(bot, update, args):
 		bot.send_message(chat_id=update.message.chat_id, reply_to_message_id=msg_ID, text="Let me open the shop up! One second please.")
 		item_list = fnite.getStore()
 		for item in item_list:
-			bot.send_message(chat_id=update.message.chat_id, reply_to_message_id=msg_ID, text=item)
+			bot.send_photo(chat_id=update.message.chat_id, reply_to_message_id=msg_ID, photo=item)
 		#bot.send_message(chat_id=update.message.chat_id, reply_to_message_id=msg_ID, text=resp)
 	else:
 		name = ""
