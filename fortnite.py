a9ecef9bab7c0849135909f9980591f5a5c533fd
MrBabalafe <walkersakatch@gmail.com>
2018-09-25 17:11:50 -0600

    Polish up the daily and weekly shop responses

--- fortnite.py
+++ fortnite.py
@@ -24,6 +24,19 @@ def getStats(name, platform="pc"):
 	resp = "{}'s Stats:\n\nSolo Kills: {}\nSolo K/D: {}\nSolo Wins: {}\n------------\nDuo Kills: {}\nDuo K/D: {}\nDuo Wins: {}\n------------\nSquad Kills: {}\nSquad K/D: {}\nSquad Wins: {}".format(epic_name, solo_kills, solo_kd, solo_wins, duo_kills, duo_kd, duo_wins, squad_kills, squad_kd, squad_wins)
 	return(resp)
 
+def getStore():
+	url = "https://api.fortnitetracker.com/v1/store"
+	API_key = os.getenv('FORTNITE_API_KEY')
+	headers = {'TRN-Api-Key' : API_key}
+	r = requests.get(url, headers=headers)
+	data = r.json()
+	resp = []
+
+	for item in data:
+		resp.append(InputMediaPhoto(media=item['imageUrl'], caption=item['name'] + " - " + str(item['vBucks']) + " V-Bucks"))
+
+	return(resp)
+
 def getWeeklyStore():
 	url = "https://api.fortnitetracker.com/v1/store"
 	API_key = os.getenv('FORTNITE_API_KEY')
@@ -34,6 +47,20 @@ def getWeeklyStore():
 
 	for item in data:
 		if(item['storeCategory'] == "BRWeeklyStorefront"):
-			resp.append(InputMediaPhoto(media=item['imageUrl'], caption=item['name'] + " - " + str(item['vBucks']) + "V-Bucks"))
+			resp.append(InputMediaPhoto(media=item['imageUrl'], caption=item['name'] + " - " + str(item['vBucks']) + " V-Bucks"))
+
+	return(resp)
+
+def getDailyStore():
+	url = "https://api.fortnitetracker.com/v1/store"
+	API_key = os.getenv('FORTNITE_API_KEY')
+	headers = {'TRN-Api-Key' : API_key}
+	r = requests.get(url, headers=headers)
+	data = r.json()
+	resp = []
+
+	for item in data:
+		if(item['storeCategory'] == "BRDailyStorefront"):
+			resp.append(InputMediaPhoto(media=item['imageUrl'], caption=item['name'] + " - " + str(item['vBucks']) + " V-Bucks"))
 
 	return(resp)
